ca464f483d2b5340da1d40ef6681166848005bab
Edward Korotaev <eduardkorotaev77@gmail.com>
2024-06-05 23:00:12 +0200

    ReadyToGo

--- Lab_5/code/index.php
+++ Lab_5/code/index.php
@@ -1,28 +1,76 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Доска объявлений</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eduardo</title>
 </head>
 <body>
-<h2>Добавить объявление</h2>
-<form action="submit.php" method="post">
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email" required><br>
+<div id="form">
+    <!-- ====== Форма добавления новых объявлений ======-->
+    <form action="save.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" required>
 
-    <label for="title">Заголовок объявления:</label><br>
-    <input type="text" id="title" name="title" required><br>
+        <label for="category">Category</label>
+        <select name="category" required>
+            <option value="cars">cars</option>
+            <option value="dogs">dogs</option>
+            <option value="games">games</option>
+            <option value="phones">phones</option>
+            <option value="others">others</option>
+        </select>
+        <label for="title">Title</label>
+        <input type="text" name="title" required>
 
-    <label for="text">Текст объявления:</label><br>
-    <textarea id="text" name="text" rows="4" required></textarea><br>
+        <label for="description">Description</label>
+        <textarea rows="10" name="description"></textarea>
 
-    <label for="category">Категория:</label><br>
-    <input type="text" id="category" name="category" required><br><br>
+        <input type="submit" value="Save">
+    </form>
+</div>
 
-    <input type="submit" value="Добавить">
-</form>
+<!-- ====== Таблица объявлений ======-->
+<div id="table">
+    <table>
+        <thead>
+        <th>Category</th>
+        <th>Title</th>
+        <th>Email</th>
+        <th>Description</th>
+        </thead>
+        <tbody>
+        <?php
+        $db_host = "db";
+        $db_user = "root";
+        $db_password = "helloworld";
+        $db_name = "web";
+
+        // Подключаемся к БД
+        $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+        // Проверка соединения
+        if ($connection->connect_error) {
+            die("Error: " . $connection->connect_error);
+        }
+
+        // Отображение объявлений
+        $result = $connection->query("SELECT * FROM web.ad");
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['category'] . "</td>";
+            echo "<td>" . $row['title'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "<td>" . $row['description'] . "</td>";
+            echo "</tr>";
+        }
+
+        $connection->close();
+        ?>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
-
-<?php
--- Lab_5/code/save.php
+++ Lab_5/code/save.php
@@ -1,1 +1,29 @@
 <?php
+$db_host = "db";
+$db_user = "root";
+$db_password = "helloworld";
+$db_name = "web";
+
+// Подключаемся к БД
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+if ($mysqli->connect_error) {
+    die("Error: " . $mysqli->connect_error);
+}
+
+// Получаем информацию
+$email = $_POST['email'];
+$category = $_POST['category'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+// Запись данных
+$stmt = $mysqli->prepare("INSERT INTO web.ad (email, category, title, description) VALUES (?, ?, ?, ?)");
+
+$stmt->bind_param("ssss", $email, $category, $title, $description);
+$stmt->execute();
+
+$stmt->close();
+$mysqli->close();
+
+header("Location: index.php");
+exit;
